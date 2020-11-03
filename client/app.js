38a380c5d471f0887668cef753bb71a37ed02688
asemnouri <64498291+asemnouri@users.noreply.github.com>
2020-11-03 14:41:04 +0200

    oh no

--- client/app.js
+++ client/app.js
@@ -1,2 +1,21 @@
+let jsonData = document.getElementById("jsonData")
+let button = document.getElementById("button")
 
+
+let data;
+
+
+
+const submit = (e) => {
+    e.preventDefault()
+
+    console.log(data)
+}
+const onChange = (e) => {
+    data = e.target.value
+
+}
+
+button.addEventListener('click', submit)
+jsonData.addEventListener("change", onChange)
 
--- server.js
+++ server.js
@@ -1,2 +1,18 @@
+const express = require('express')
+
+const app = express()
+
+app.use(express.urlencoded({ extended: false }))
+app.use(express.json())
+
+app.use(express.static(__dirname + '/./client'));
 
 
+app.post('/json' , (req,res) => {
+    console.log(req.body)
+    
+})
+
+app.listen(3000,() => {
+    console.log(' listening ')
+})
